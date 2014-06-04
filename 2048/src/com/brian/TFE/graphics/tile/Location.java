b5afd5d32ab9659d298be6174bada193f60502e7
brianlevis <brianlevis1@gmail.com>
2014-06-04 10:11:41 -0400

    now can tell if positions have been assigned

--- 2048/src/com/brian/TFE/graphics/tile/Location.java
+++ 2048/src/com/brian/TFE/graphics/tile/Location.java
@@ -2,9 +2,18 @@ package com.brian.TFE.graphics.tile;
 
 public class Location {
 
-	int xPosition, yPosition;
-	
+	public int xPosition = -1;
+	public int yPosition = -1;
+
+	public Location() {
+	}
+
 	public Location(int xPosition, int yPosition) {
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+
+	public void setLocation(int xPosition, int yPosition) {
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
 	}
