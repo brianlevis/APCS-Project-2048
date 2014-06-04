75c728f340d9041178705c8c4003f631f61dc0fa
brianlevis <brianlevis1@gmail.com>
2014-06-04 09:59:49 -0400

    somewhat of a messy solution but whatever

---
+++ 2048/src/com/brian/TFE/graphics/tile/Location.java
@@ -0,0 +1,12 @@
+package com.brian.TFE.graphics.tile;
+
+public class Location {
+
+	int xPosition, yPosition;
+	
+	public Location(int xPosition, int yPosition) {
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+
+}
